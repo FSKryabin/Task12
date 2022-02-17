906e2934e1c27722e74434ea5c01dfdb153c22fd
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2022-02-17 16:47:48 +0300

    Добавьте файлы проекта.

---
+++ Task12/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+	static class Circle
+	{
+		public static void GetСircumference(double Radius)
+		{
+			double Сircumference = Math.PI * 2 * Radius;
+			Console.WriteLine("Длина окружности {0:f2}", Сircumference);
+		}
+		public static void GetSquare(double Radius)
+		{
+			double Square = Math.PI * Math.Pow(Radius, 2.0);
+			Console.WriteLine("Площадь круга {0:f2}", Square);
+		}
+		public static void GetPosition(double Radius, double positionX, double positionY)
+		{
+			if (positionX <= Radius && positionY <= Radius)
+			{
+				Console.WriteLine("Точка с координатами x = {0}, y = {1} принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
+			}
+			else
+			{
+				Console.WriteLine("Точка с координатами x = {0}, y = {1} не принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
+			}
+		}
+	}
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			try
+			{
+				Console.Write("Введите радиус: ");
+				double Radius = Convert.ToDouble(Console.ReadLine());
+				if (Radius <= 0)
+				{
+					Console.Write("Ошибка.");
+					return;
+				}
+				Console.Write("Введите координату Х: ");
+				double positionX = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+				Console.Write("Введите координату Y: ");
+				double positionY = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+				Circle.GetСircumference(Radius);
+				Circle.GetSquare(Radius);
+				Circle.GetPosition(Radius, positionX, positionY);
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Неверный ввод.");
+				return;
+			}
+		}
+	}
+}
